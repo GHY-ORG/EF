d607bb43f5bf86ac6bb985298be640cfb6f262ce
Yuxuan <cookie@ghy.cn>
2017-04-26 22:49:52 +0800

    split authentication configures

--- src/main/java/cn/ghy/WebSecurityConfig.java
+++ src/main/java/cn/ghy/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import cn.ghy.authentication.EfUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,37 +15,41 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/api/**")
+                    .authorizeRequests()
+                    .anyRequest().hasRole("ADMIN")
+                    .and()
+                    .httpBasic();
+        }
+    }
+
+    @Configuration
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login.html").permitAll()
+                    .failureUrl("/login-error.html");
+        }
+    }
+
     @Autowired
     private EfAuthenticationProvider provider;
 
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
-//        manager.createUser(User.withUsername("admin").password("password").roles("USER","ADMIN").build());
-//        return manager;
-//    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/css/**", "/index").permitAll()
-//                    .antMatchers("/user/**").hasRole("USER")
-                    .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login.html").permitAll()
-                    .failureUrl("/login-error.html")
-                .and()
-                .httpBasic();
-    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
